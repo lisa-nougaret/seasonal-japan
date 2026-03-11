70862ac579b790c23dead6f492bd3b66df3487af
Lisa Nougaret <lisa.nougaret@outlook.fr>
2026-03-11 17:01:06 +0100

    Add dim_station dimension table

---
+++ sql/analytics/dim_station.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS analytics.dim_station;
+
+CREATE TABLE analytics.dim_station AS
+
+SELECT
+    station_code,
+    station_name,
+    MIN(month_start_date) AS first_observation,
+    MAX(month_start_date) AS last_observation,
+    COUNT(*) AS record_count
+FORM analytics.fact_monthly_climate
+GROUP BY
+    station_code,
+    station_name;
+ORDER BY station_code;
